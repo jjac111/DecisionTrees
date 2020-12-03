0362a445425fb32db6f6187dffc5ef09f7dba13a
Juan J Arosemena <jarosemena@libelulasoft.com>
2020-12-03 11:10:46 -0500

    Finished Up

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+
+
 features_to_remove = [38, 35, 11, 27, 17, 7, 12, 32, 9, 36]
 
 data_path = 'data/'
@@ -6,3 +8,5 @@ train_filename = data_path + 'SPECTF.train'
 test_filename = data_path + 'SPECTF.test'
 
 ten_fold_data_path = data_path + '10-fold/'
+
+
